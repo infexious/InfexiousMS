13e12d68b479b4b68bff383ecb3597758dcd1023
infexious <cdawgfx@live.com>
2012-08-13 21:54:30 -0500

    Update src/constants/ServerConstants.java

--- src/constants/ServerConstants.java
+++ src/constants/ServerConstants.java
@@ -19,6 +19,8 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+//BANANA
 package constants;
 
 public class ServerConstants {
